44bda6339bbecb3723cf467142e5b80bc90cce0a
ahin0422 <ahinahn0422@gmail.com>
2026-06-16 20:38:28 +0900

    add

---
+++ 6055.py
@@ -0,0 +1,6 @@
+a,b = map(int,input().split())
+a = bool(a) ; b = bool(b)
+if a == True or b == True:
+    print("True")
+else :
+    print("False")
---
+++ 6056.py
@@ -0,0 +1,6 @@
+a,b = map(int,input().split())
+a = bool(a) ; b = bool(b)
+if a != b:
+    print("True")
+else :
+    print("False")
